275e99540b1442762e063d5001c14a25744a5c90
sergiolbqr <sergio.gomesc13@outlook.es>
2026-01-30 12:46:12 -0500

    feat: implementar navegación suave en el menú y corregir enlaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { useRoutes } from "react-router-dom";
 import { appRoutes } from "./routes/app.routes";
 import type { JSX } from "react";
+import ScrollToHashElement from "./components/ScrollToHashElement";
 
 export default function App(): JSX.Element | null {
   const element = useRoutes(appRoutes);
-  return element;
+  return (
+    <>
+      <ScrollToHashElement />
+      {element}
+    </>
+  );
 }
